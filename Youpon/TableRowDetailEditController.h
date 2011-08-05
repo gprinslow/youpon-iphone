4009ea1c42f5dd13759e0d4988f27f657034d5fe
Garrison Prinslow <gprinslow@gmail.com>
2011-08-04 21:22:11 -0500

    Add abstract Table Row Detail Editor class

---
+++ Youpon/TableRowDetailEditController.h
@@ -0,0 +1,25 @@
+//
+//  TableRowDetailEditController.h
+//  Youpon
+//
+//  Created by Garrison on 8/4/11.
+//  Copyright 2011 Garrison Prinslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define kNonEditableTextColor [UIColor colorWithRed:0.318 green:0.4 blue:0.569 alpha:1.0]
+
+@interface TableRowDetailEditController : UITableViewController {
+    NSMutableDictionary *data;
+    NSString *keypath;
+    NSString *rowLabel;
+}
+
+@property (nonatomic, retain) NSMutableDictionary *data;
+@property (nonatomic, retain) NSString *keypath;
+@property (nonatomic, retain) NSString *rowLabel;
+
+-(IBAction)cancel;
+-(IBAction)saveEditedDataLocally;
+
+@end
