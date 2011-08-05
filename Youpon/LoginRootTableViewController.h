3ded5c2d7a2d6edcd623e42af81d0cfa2865ad22
Garrison Prinslow <gprinslow@gmail.com>
2011-08-05 13:03:21 -0500

    Add login root table view controller

---
+++ Youpon/LoginRootTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginRootTableViewController.h
+//  Youpon
+//
+//  Created by Garrison on 8/5/11.
+//  Copyright 2011 Garrison Prinslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GroupedEditTableViewController.h"
+
+@interface LoginRootTableViewController : GroupedEditTableViewController {
+    
+}
+
+@end
