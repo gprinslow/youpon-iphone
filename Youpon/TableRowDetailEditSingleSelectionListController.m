b79a8c970dc13b9917fe233787b46fd16b4ce9af
Garrison Prinslow <gprinslow@gmail.com>
2011-08-05 12:15:11 -0500

    Add detail single selection list editor

---
+++ Youpon/TableRowDetailEditSingleSelectionListController.m
@@ -0,0 +1,168 @@
+//
+//  TableRowDetailEditSingleSelectionListController.m
+//  Youpon
+//
+//  Created by Garrison on 8/5/11.
+//  Copyright 2011 Garrison Prinslow. All rights reserved.
+//
+
+#import "TableRowDetailEditSingleSelectionListController.h"
+
+
+@implementation TableRowDetailEditSingleSelectionListController
+
+@synthesize list;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [list release];
+    [lastIndexPath release];
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    NSString *currentValue = [self.data valueForKeyPath:self.keyPath];
+    
+    for (NSString *oneItem in list) {
+        if ([oneItem isEqualToString:currentValue]) {
+            NSUInteger newIndex[] = {0, [list indexOfObject:oneItem]};
+            NSIndexPath *newPath = [[NSIndexPath alloc] initWithIndexes:newIndex length:2];
+            [lastIndexPath release];
+            lastIndexPath = newPath;
+            break;
+        }
+    }
+    
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [list count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *TableRowDetailEditSingleSelectionListControllerCellIdentifier = @"TableRowDetailEditSingleSelectionListControllerCellIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:TableRowDetailEditSingleSelectionListControllerCellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:TableRowDetailEditSingleSelectionListControllerCellIdentifier] autorelease];
+    }
+    
+    // Configure the cell...
+    NSUInteger row = [indexPath row];
+    NSUInteger oldRow = [lastIndexPath row];
+    cell.textLabel.text = [list objectAtIndex:row];
+    cell.accessoryType = (row == oldRow && lastIndexPath != nil) ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;    
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+    int newRow = [indexPath row];
+    int oldRow = [lastIndexPath row];
+    
+    if (newRow != oldRow || newRow == 0) {
+        UITableViewCell *newCell = [tableView cellForRowAtIndexPath:indexPath];
+        newCell.accessoryType = UITableViewCellAccessoryCheckmark;
+        
+        UITableViewCell *oldCell = [tableView cellForRowAtIndexPath:lastIndexPath];
+        oldCell.accessoryType = UITableViewCellAccessoryNone;
+        
+        [lastIndexPath release];
+        lastIndexPath = indexPath;
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - Super class overrides
+- (IBAction)saveEditedDataLocally {
+    UITableViewCell *selectedCell = [self.tableView cellForRowAtIndexPath:lastIndexPath];
+    NSString *newValue = selectedCell.textLabel.text;
+    
+    [self.data setValue:newValue forKeyPath:self.keyPath];
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
